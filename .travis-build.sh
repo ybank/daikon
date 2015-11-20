5cc54b3e3a880bdeae610adcf5d44d509672251b
Mark Roberts <markro@cs.washington.edu>
2015-11-20 11:40:20 -0800

    test building kvasir

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,7 +7,11 @@ export DAIKONDIR=`pwd`
 
 # Same as in Jenkins; should abstract out
 # make -C java very-clean
-make showvars compile daikon.jar javadoc
-make -C doc
-make -C java dcomp_rt.jar
-make -C tests all
+#make showvars compile daikon.jar javadoc
+#make -C doc
+#make -C java dcomp_rt.jar
+#make -C tests all
+#git clone https://github.com/codespecs/fjalar.git fjalar
+make kvasir
+#make -C tests/kvasir-tests nightly-summary-w-daikon
+
